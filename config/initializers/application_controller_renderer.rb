c1dcf37ea1c033b531bdc4cc0cd110626b7e39f9
techss22 <jemus250@gmail.com>
2020-12-09 23:37:59 +0900

    user_setting

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eecaf2f0ac44393dda144567a0344f0d2f4dfa23efb2557e68d4bafeb392dc0863a319356ead0cf8e8c2a54092211217ca25219258c6139085b960a68d75deeb'
