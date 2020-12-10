077ea56650ed259d5eb21835e8cba7cb22b519b1
techss22 <jemus250@gmail.com>
2020-12-10 18:55:29 +0900

    update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def index
+    
   end
 
   def show
@@ -19,6 +20,12 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
 
   private
